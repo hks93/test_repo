db8eafd37db2245f6d01b3f52f787258f1db13bf
hks93 <38750028+hks93@users.noreply.github.com>
2019-04-04 13:39:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+import CMArea from './cm-area';
+import CMAreaMedian from './cm-area-median';
+import CMAreaStacked from './cm-area-stacked';
+import CMBar from './cm-bar';
+import CMBarStacked from './cm-bar-stacked';
+import CMButterfly from './cm-butterfly';
+import CMBubble from './cm-bubble';
+import CMColumn from './cm-column';
+import CMColumnStacked from './cm-column-stacked';
+import CMConcentricRings from './cm-concentric-rings';
+
+import CMDonut from './cm-donut';
+import CMDonutPie from './cm-donut-pie';
+import CMGaugeMeter from './cm-gauge';
+// import CMGaugeMeter from './cm-gauge-meter';
+import CMHalfRings from './cm-half-rings';
+import CMLine from './cm-line';
+import CMLineStacked from './cm-line-stacked';
+import CMMultipleColumns from './cm-multiple-columns';
+import CMNumbers from './cm-numbers';
+import CMScatter from './cm-scatter';
+import CMText from './cm-text';
+import CMImage from './cm-image';
+
+// import CMPie from './cm-pie';
+import CMCanvasSummary from './cm-canvas-summary';
+import CMLollipop from './cm-lollipop';
+import CMStackedBarGraphElement from './cm-stacked-bar-graph';
+import CMSummaryList from './cm-summary-list';
+import CMCanvasColumn from './cm-canvas-column';
+import CMTreemap from './cm-treemap';
+import CMFootprint from './cm-footprint';
+import CMRectBar from './cm-rect-bar';
+import CMDataTreeColumns from './cm-datatree-columns';
+import CMAvailability from './cm-availablity';
+import CMHeatMap from './cm-heatmap';
+import CMAvailabilityL1 from './cm-availability-status';
+// import CMCanvasMultilineSummary from './cm-canvas-multiline-summary'; // Graph is specific to canvas
+
+export {
+  CMArea,
+  CMAreaMedian,
+  CMAreaStacked,
+  CMBar,
+  CMBarStacked,
+  CMBubble,
+  CMColumn,
+  CMConcentricRings,
+  CMDonut,
+  CMDonutPie,
+  // CMGauge,
+  CMGaugeMeter,
+  CMHalfRings,
+  CMLine,
+  CMLineStacked,
+  CMMultipleColumns,
+  CMNumbers,
+  CMScatter,
+  CMText,
+  CMImage,
+  CMColumnStacked,
+  // New Widgets From Canvas,
+  // CMPie,
+  CMCanvasSummary,
+  CMLollipop,
+  CMStackedBarGraphElement,
+  CMSummaryList,
+  CMCanvasColumn,
+  CMTreemap,
+  CMButterfly,
+  CMFootprint,
+  CMRectBar,
+  CMDataTreeColumns,
+  CMAvailability,
+  CMHeatMap,
+  CMAvailabilityL1,
+
+  // CMCanvasMultilineSummary,
+};
